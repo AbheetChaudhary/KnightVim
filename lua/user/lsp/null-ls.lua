e7c157dea490ff73922cad2a76685d41a590ad02
abheetChaudhary <abheetchaudhary25@gmail.com>
2022-11-04 02:24:16 +0530

    Configure clangd to solve multiple offset_encoding error. Update README.md and LICENSE. Other minor changes.

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-  return
+	return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -8,13 +8,19 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
-null_ls.setup {
-  debug = false,
-  sources = {
-    formatting.prettier.with { extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } },
-    formatting.black.with { extra_args = { "--fast" } },
-    -- formatting.yapf,
-    formatting.stylua,
-    diagnostics.flake8.with { extra_args = { "--extend-ignore=F821,E302,E501"}},
-  },
-}
+null_ls.setup({
+	-- -- Global offset_encoding for null-ls
+	-- on_init = function(new_client, _)
+	-- new_client.offset_encoding = "utf-8"
+	-- end,
+
+	debug = false,
+	sources = {
+		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+		formatting.black.with({ extra_args = { "--fast" }, filetype = { "python" } }),
+		-- formatting.yapf,
+		formatting.stylua,
+		diagnostics.flake8.with({ extra_args = { "--extend-ignore=F821,E302,E501" }, filetype = { "python" } }),
+		formatting.clang_format,
+	},
+})
