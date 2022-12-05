52c7e18c18ce3eb9690f4b88d8213385f3565d0a
abheetChaudhary <abheetchaudhary25@gmail.com>
2022-12-05 07:27:55 +0530

    Add visual mode keybindings in 'Hop'

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -105,7 +105,6 @@ return packer.startup(function(use)
 	use({
 		"nvim-treesitter/nvim-treesitter",
 		run = ":TSUpdate",
-		commit = "5d11dfc27849e3d7435c32dbd8ea9be35bcd990b", -- treesitter syntax highlighting didnt looked as beautiful after this version. Comments etc were all white instead of green
 	})
 	use("p00f/nvim-ts-rainbow")
 
