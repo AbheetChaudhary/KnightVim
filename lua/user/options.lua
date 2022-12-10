76f3f70d4d64570b95f134604dc8b14ee53197a3
abheetChaudhary <abheetchaudhary25@gmail.com>
2022-12-10 15:29:20 +0530

    Turn of linewrap.

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -60,7 +60,7 @@ end
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
 -- vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
-vim.cmd "set wrap linebreak"
+-- vim.cmd "set wrap linebreak"
 
 vim.filetype.add {
   extension = {
