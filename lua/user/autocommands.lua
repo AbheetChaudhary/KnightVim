b9d47fd5cc70b87a5596eeef9fb1da7cd6672834
Abheet Chaudhary <81126136+AbheetChaudhary@users.noreply.github.com>
2023-05-21 12:32:03 +0530

    Translate autocommands.lua to use neovim lua api
    Change the method by which autocommands are created which is in tune
    with the neovim lua api's to set autocommands. Some commented
    autocommands are still kept as I have still not decided to use them.

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,58 +1,47 @@
-vim.cmd([[
-  augroup _general_settings
-    autocmd!
-    autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR> 
-    autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200}) 
-    autocmd BufWinEnter * :set formatoptions-=cro | :set showtabline=1
-    autocmd FileType qf set nobuflisted
-  augroup end
+local _general_settings = vim.api.nvim_create_augroup("_general_settings", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {pattern = { "qf", "help", "man", "lspinfo" }, command = "nnoremap <silent> <buffer> q :close<CR>", group = _general_settings })
+vim.api.nvim_create_autocmd("FileType", {pattern = { "qf" }, command = "set nobuflisted", group = _general_settings })
+vim.api.nvim_create_autocmd("TextYankPost", {pattern = { "*" }, command = "silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200})", group = _general_settings })
+vim.api.nvim_create_autocmd("BufWinEnter", {pattern = { "*" }, command = ":set formatoptions-=cro | :set showtabline=1", group = _general_settings })
 
-  augroup _git
-    autocmd!
-    autocmd FileType gitcommit setlocal wrap
-    autocmd FileType gitcommit setlocal spell
-  augroup end
+local _git = vim.api.nvim_create_augroup("_git", { clear = true })
+vim.api.nvim_create_autocmd("FileType", { pattern = "gitcommit", command = "setlocal wrap", group = _git })
+vim.api.nvim_create_autocmd("FileType", { pattern = "gitcommit", command = "setlocal spell", group = _git })
 
-  augroup _markdown
-    autocmd!
-    autocmd FileType markdown setlocal wrap
-    autocmd FileType markdown setlocal spell
-  augroup end
+local _markdown = vim.api.nvim_create_augroup("_markdown", { clear = true })
+vim.api.nvim_create_autocmd("FileType", { pattern = "markdown", command = "setlocal wrap | setlocal spell", group = _markdown })
 
-  augroup _auto_resize
-    autocmd!
-    autocmd VimResized * tabdo wincmd = 
-  augroup end
+--   augroup _auto_resize
+--     autocmd!
+--     autocmd VimResized * tabdo wincmd = 
+--   augroup end
 
-  augroup _alpha
-    autocmd!
-    autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
-  augroup end
+local _alpha = vim.api.nvim_create_augroup("_alpha", { clear = true })
+vim.api.nvim_create_autocmd("User", { pattern = "AlphaReady", command = "set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2", group = _alpha })
 
-" if has_key(environ(), 'TMUX')
-"   augroup tmux_something
-"     autocmd VimResume  * call system('tmux set status off')
-"     autocmd VimEnter   * call system('tmux set status off')
-"     autocmd VimLeave   * call system('tmux set status on')
-"     autocmd VimSuspend * call system('tmux set status on')
-"   augroup END
-" endif
+-- " if has_key(environ(), 'TMUX')
+-- "   augroup tmux_something
+-- "     autocmd VimResume  * call system('tmux set status off')
+-- "     autocmd VimEnter   * call system('tmux set status off')
+-- "     autocmd VimLeave   * call system('tmux set status on')
+-- "     autocmd VimSuspend * call system('tmux set status on')
+-- "   augroup END
+-- " endif
 
-"   augroup collumnLimit
-"   autocmd!
-"   autocmd BufEnter,WinEnter,FileType scala,java,python
-"         \ highlight CollumnLimit ctermbg=DarkGrey guibg=DarkGrey
-"   let collumnLimit = 80 " feel free to customize
-"   let pattern =
-"         \ '\%<' . (collumnLimit+1) . 'v.\%>' . collumnLimit . 'v'
-"   autocmd BufEnter,WinEnter,FileType scala,java,python
-"         \ let w:m1=matchadd('CollumnLimit', pattern, -1)
-" augroup END
+-- "   augroup collumnLimit
+-- "   autocmd!
+-- "   autocmd BufEnter,WinEnter,FileType scala,java,python
+-- "         \ highlight CollumnLimit ctermbg=DarkGrey guibg=DarkGrey
+-- "   let collumnLimit = 80 " feel free to customize
+-- "   let pattern =
+-- "         \ '\%<' . (collumnLimit+1) . 'v.\%>' . collumnLimit . 'v'
+-- "   autocmd BufEnter,WinEnter,FileType scala,java,python
+-- "         \ let w:m1=matchadd('CollumnLimit', pattern, -1)
+-- " augroup END
 
- autocmd BufRead,BufNewFile *.sage,*.pyx,*.spyx set filetype=python
- autocmd Filetype python set tabstop=4|set shiftwidth=4|set expandtab
- autocmd FileType python set makeprg=sage\ -b\ &&\ sage\ -t\ %
-]])
+local _python = vim.api.nvim_create_augroup("_python", { clear = true })
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, { pattern = { "*.sage", ".pyx", "*.spyx" }, command = "set filetype=python", group = _python })
+vim.api.nvim_create_autocmd("FileType", { pattern = "python", command = "set tabstop=4 | set shiftwidth=4 | set expandtab | set makeprg=sage -b && sage -t %", group = _python })
 
 -- Autoformat
 -- augroup _lsp
